de6e1e1889dd5266de051693ae2c7d5eb55fe509
Lasen Kulanaka <147847779+Lasen-Kulanaka@users.noreply.github.com>
2024-10-13 23:45:55 +0530

    Create LW1Q4.java

---
+++ LW1Q4.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Q4 {
+    public static void main(String[] args){
+        JFrame myWindow = new JFrame();
+        myWindow.setSize(300,200);
+        myWindow.setTitle("My First Frame");
+        myWindow.setVisible(true);
+        myWindow.setLocation(100,50);
+    }
+}
